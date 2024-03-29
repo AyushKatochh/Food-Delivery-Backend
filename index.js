c86bcd169454a7de49f4274996f83a2a2a25f2e8
AyushKatochh <katochayushhp@gmail.com>
2024-03-30 02:22:43 +0530

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
+import cors from "cors";
 import organizationRoutes from './routes/organizationRoutes.js';
 import deliveryRoutes from './routes/deliveryRoutes.js';
 import { options as swaggerOptions } from './swaggerOptions.js';
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
